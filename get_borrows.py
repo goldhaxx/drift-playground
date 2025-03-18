d4b2215fb7ee714aa946a4433c01502eafd9be02
goldhaxx <1616671+goldhaxx@users.noreply.github.com>
2025-03-18 16:33:59 -0700

    Add get_borrows.py for retrieving borrow and deposit rates
    This new script connects to Drift using the driftpy-sdk and retrieves the borrow and deposit rates for a specified market. It utilizes asynchronous operations to fetch market data, calculates various financial metrics such as borrow rate, deposit rate, utilization, and interest rate, and formats the output for clarity. The script is designed to be run directly and loads environment variables from a .env file for configuration.

---
+++ get_borrows.py
@@ -0,0 +1,59 @@
+# Taken from driftpy repository
+# https://github.com/drift-labs/driftpy/blob/master/examples/get_borrows.py
+# This script will get the borrow and deposit rates for a given market
+
+import asyncio
+import os
+
+from anchorpy.provider import Wallet
+from dotenv import load_dotenv
+from solana.rpc.async_api import AsyncClient
+
+from driftpy.accounts.get_accounts import get_spot_market_account
+from driftpy.drift_client import DriftClient
+from driftpy.math.spot_balance import (
+    calculate_borrow_rate,
+    calculate_deposit_rate,
+    calculate_interest_rate,
+    calculate_utilization,
+)
+from driftpy.math.spot_market import get_token_amount
+from driftpy.types import SpotBalanceType
+
+
+async def main():
+    load_dotenv()
+    url = os.getenv("RPC_URL")
+    connection = AsyncClient(url)
+    dc = DriftClient(connection, Wallet.dummy(), "mainnet")
+    market = await get_spot_market_account(dc.program, 0)  # USDC
+    if market is None:
+        raise Exception("No market found")
+    token_deposit_amount = get_token_amount(
+        market.deposit_balance,
+        market,
+        SpotBalanceType.Deposit(),  # type: ignore
+    )
+
+    token_borrow_amount = get_token_amount(
+        market.borrow_balance,
+        market,
+        SpotBalanceType.Borrow(),  # type: ignore
+    )
+    print(f"token_deposit_amount: {(token_deposit_amount/10**market.decimals):,.2f}")
+    print(f"token_borrow_amount: {(token_borrow_amount/10**market.decimals):,.2f}")
+
+    borrow_rate = calculate_borrow_rate(market)
+    deposit_rate = calculate_deposit_rate(market)
+    utilization = calculate_utilization(market)
+    interest_rate = calculate_interest_rate(market)
+
+    precision = 10000
+    print(f"borrow_rate: {borrow_rate/precision:.2f}%")
+    print(f"deposit_rate: {deposit_rate/precision:.2f}%")
+    print(f"utilization: {utilization/precision:.2f}%")
+    print(f"interest_rate: {interest_rate/precision:.2f}%")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
